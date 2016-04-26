eefc6c469eba41f008485b824c4823f4ad066277
Juozas Kripas <2005.joe@gmail.com>
2016-04-26 11:37:57 -0600

    JS adding more values

--- common.js
+++ common.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-	$("#contactname").val("Please enter your full name");	
+	$("#contactname").val("Please enter your full name");
+    $("#telephone").val("Incl local dialing code");
+    $("#eventdate").val("Format DDMMYY");
+    $("#details").val("The more you can enter here, the less we have to check with you ;)");
+        
 });
